1e80cdd4cfd213122e9ceceee5bf5b5544ee3242
chandra sekhar <70020942+Achandu123@users.noreply.github.com>
2021-10-03 17:14:05 +0530

    Add files via upload

---
+++ ExArray1.java
@@ -0,0 +1,14 @@
+
+class ExArray1
+{
+   public static void main(String[] args)
+   { 
+        int i=1;
+        // int rno[]=new int[3];
+        int[] rno=new int[3];
+        rno[0]=10;
+        rno[i]=20;
+        rno[4+5-7]=30;
+     System.out.println(rno[2]);
+   }
+}   
